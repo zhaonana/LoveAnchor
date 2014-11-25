ad3c867e70ffe90aa398b163c962e6915d39deb3
NaNa <zhao_na_na@126.com>
2014-11-25 10:20:22 +0800

    添加DateUtil

---
+++ LoveAnchor/DateUtil.h
@@ -0,0 +1,15 @@
+//
+//  DateUtil.h
+//  LoveAnchor
+//
+//  Created by NaNa on 14/11/25.
+//  Copyright (c) 2014年 zhongqinglongtu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DateUtil : NSObject
+
++ (NSString *)getTimeInterval:(NSTimeInterval)time;
+
+@end

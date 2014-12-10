4f64ce8f8ecf9e7e46a2e7a8676c4aaf9af7c796
NaNa <zhao_na_na@126.com>
2014-12-10 11:46:11 +0800

    聊天服务器修改

--- LoveAnchor/Common.h
+++ LoveAnchor/Common.h
@@ -10,5 +10,5 @@
 //#define BaseURL @"http://ttapi.app1101815070.twsapp.com/" //测试服
 #define BaseURL @"http://ttapi.app1101168695.twsapp.com/" //正式服
 
-//#define BaseHost @"ttapi.app1101815070.twsapp.com"   //测试服
-#define BaseHost @"ttapi.app1101168695.twsapp.com"   //正式服
+//#define BaseHost @"ttwsshowd.app1101815070.twsapp.com"   //测试服
+#define BaseHost @"ttwsshowd.app1101168695.twsapp.com"   //正式服

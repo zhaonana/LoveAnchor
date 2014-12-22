511ab12a0b794617f99072e664454dd3b53074b5
NaNa <zhao_na_na@126.com>
2014-12-22 16:36:12 +0800

    pic修改

--- LoveAnchor/DynamicTableViewCell.m
+++ LoveAnchor/DynamicTableViewCell.m
@@ -7,6 +7,7 @@
 //
 
 #import "DynamicTableViewCell.h"
+#import "UIImageView+BoundsAdditions.h"
 
 @implementation DynamicTableViewCell
 
@@ -15,9 +16,7 @@
     self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
     if (self) {
         self.headImageView = [[UIImageView alloc]initWithFrame:CGRectMake(10, 6, 45, 45)];
-        self.headImageView.layer.borderColor = [UIColor clearColor].CGColor;
-        self.headImageView.layer.borderWidth = 5.0;
-        self.headImageView.layer.cornerRadius = 25.0;
+        [self.headImageView makeBoundImage];
         [self.contentView addSubview:self.headImageView];
         
         self.nikeNameLabel = [[UILabel alloc]initWithFrame:CGRectMake(60, 28, 200, 18)];
--- LoveAnchor/RankingModel.h
+++ LoveAnchor/RankingModel.h
@@ -16,6 +16,8 @@ typedef enum {
 
 @interface RankingModel : NSObject
 //头像
+@property (nonatomic, strong) NSString     *pic;
+//主播
 @property (nonatomic, strong) NSString     *pic_url;
 //ID
 @property (nonatomic, strong) NSNumber     *_id;
